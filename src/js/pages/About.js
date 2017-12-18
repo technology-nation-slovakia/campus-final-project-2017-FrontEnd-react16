c0c136ce8c453773660bcd197eefbf9b7f83736b
Lukáš Bihary <lukasbihary51@gmail.com>
2017-12-18 18:59:19 +0000

    About section modification

--- src/js/pages/About.js
+++ src/js/pages/About.js
@@ -4,18 +4,18 @@ const About = () => (
   <div className="text">
     <h2>About</h2>
     <p>
-    Welcome to our awesome app, which shows all events happening in 0100 Campus (the best co-working space in Bratislava) in real-time.
-    Would you like to know where and when you can arrange your next business meeting or what event will take place next in coffe veniue in nice visual way?
+    Welcome to our awesome app, which shows all events happening in 0100 Campus (the best co-working space in Bratislava, Slovakia) in real-time.
+    Would you like to know where and when you can arrange your next business meeting or what event will take place next in coffee veniue in nice visual way?
     All you have to do is just look at the TV in this co-working space and you will find all the answers! Yes, just like that!</p>
 
     <p>This app is our way of saying: "Thank you!" to 0100 Campus for sheltering us in their awesome eviroment, where our coding lessons took place.</p>
     <p>Wait... What coding lessons you ask?</p>
     <p>Well, thank you for asking!</p>
-    <p>6 months ago (thanks to organisation called BrainBasket) we sign up for cours called CS50 (which is computer science course and is part of the semester in Harvard University) and ou boy we leaned a lot!
-    We started our jurney by poking around in Scratch (yes, that coding tool for kids) all the way up the ladder to the final project of this cours, which is to your surprise, this application!
-    It was a long long journey full of fight against beasts with sharp claws – widely known as bugs. We've been consulitng fight tactics together twice a week in meetings
-    with our generals: <b>Igor Ianishevskyi</b> and <b>Eduard Shlepetskyy</b>, who made this journey much easier for us.
-    Some of us fell in the battefield, some of survived heavily wounded and some of us won this fight with glory and just tiny scratches. Those who made it: <b>Dušan Jankovič, Martina Kusnierova, Lukáš Bihary,
+    <p>6 months ago (thanks to organisation called BrainBasket) we sign up for course called CS50 (which is computer science course and is part of the semester in Harvard University) and ou boy we leaned a lot!
+    We started our journey by poking around in Scratch (yes, that coding tool for kids) all the way up the ladder to the final project, which is to your surprise, this application!
+    It was a long long journey full of fight against beasts with sharp claws – widely known as bugs. We've been consulitng fight tactics together twice a week
+    with our generals: <b>Igor Ianishevskyi</b> and <b>Eduard Shlepetskyy</b>, who made it much easier for us.
+    Some of us fell in the battefield, some survived heavily wounded and some of us won this fight with glory and just tiny scratches. Those who made it: <b>Dušan Jankovič, Martina Kušnierová, Lukáš Bihary,
     Vladimir Kargapolov and Matej Mikuška</b> are armed for the future with new coding weapons like Python, Flask, SQL, HTML or Javascript. </p>
     <p>So be aware, we are ready to use them!</p>
     <div className="image"></div>
